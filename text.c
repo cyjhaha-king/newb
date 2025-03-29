69587e79ed0bbe121c97f20bc7c3c9e7bbee9137
cyjhaha-king <1277152239@qq.com>
2025-03-29 18:23:08 +0800

    Add files via upload

--- text.c
+++ text.c
@@ -1,17 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
 	int i = 1;
-	while (i <= 10)
+	int n = 0;
+	int ret = 1;
+	int sum = 0;
+
+	for (n = 1; n <= 3; n++)
 	{
-		i++;
-		if (5 == i)
-			continue;
-
-		printf("%d ", i);
-
+		ret = 1;
+		for (i = 1; i <= n; i++)
+		{
+			ret = ret * i;
+		}
+		sum = sum + ret;
 	}
+
+	printf("%d\n", sum);
 
 	return 0;
 }
