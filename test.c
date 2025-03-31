1db3ef033c1b1133c5e63938ec268df87ab5c6b5
cyjhaha-king <1277152239@qq.com>
2025-03-31 18:57:37 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,39 +1,36 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include<stdio.h>
+//数组，用来存储相同类型元素的合集
+//int main()
+//{
+//	int score[10];//定义整形数组，静态定义数组
+//	score[0] = 150;//为数组下标0元素赋值
+//	for (int i = 0; i < 10; i++)//为数组赋值
+//	{
+//		scanf("%d", &score[1]);//输入函数
+//	}
+//	for (int i = 0; i < 10; i++)//打印数组中的元素
+//	{
+//		printf("%d", score[i]);
+//	}
+//
+//	return 0;
+//}
+#define MAXSIZE 100 //定义符号define
 int main()
 {
-	///*int ch = 0;
-	//while(getchar())*///getchar:获取字符(从键盘上获取一个字符，即运行时输入的值)，get char 
-	//int ch = getchar();//getchar获取的值赋值给ch
-	//printf("%c\n", ch);//打印ch(1
-	//putchar(ch);//打印ch(2
-
-	//return 0;
-
-	//int ch = 0;
-	//while ((ch = getchar()) != EOF)
-	//{
-	//	putchar(ch);
-	//}//输入ctrl+Z结束代码
-
-	char password[20] = { 0 };
-	printf("请输入密码：>");
-	scanf("%s", password );//不读空格
-	//getchar();//读取走了\n
-	int ch = 0;
-	while ((ch = getchar()) != '\n')
+	int A[10];//静态数组A
+	int n;//定义整型变量n，记录数组B元素个数
+	scanf("%d", &n);//输入B元素个数
+	int* B = (int* )malloc(sizeof(int) * n);//动态定义数组B
+	char C[MAXSIZE];//静态定义数组C
+	for (int i = 0; i < 10; i++)//初始化数组A
 	{
-		;
+		A[i] = i;
 	}
-	printf("请确认密码（Y/N）:>");
-	int ret = getchar();
-	if ('Y' == ret)
-	{
-		printf("Yes\n");printf
-	}
-	else
-	{
-		printf('No\n');
-	}
-		return 0;
+	free(B);
+	return 0;
 }
+
+
+
